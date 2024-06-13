03f5fab202408e401d837ad22de9d55d05baf6f8
vishv183 <vishvmunjapara23@gmail.com>
2024-06-13 11:43:07 +0530

    added created_at field in table watch

--- Watches/models.py
+++ Watches/models.py
@@ -1,4 +1,5 @@
 from django.db import models
+from import_export import resources
 
 # Create your models here.
 
@@ -7,21 +8,28 @@ movement_type = [
     ('Manual', 'Manual'),
     ('Quartz', 'Quartz')
 ]
+
+
 class Watch(models.Model):
     brand = models.CharField(max_length=255)
-    model = models.CharField(max_length=255, blank=True, null=True)
-    case_materal = models.CharField(max_length=100)
+    model = models.CharField(max_length=255)
+    case_material = models.CharField(max_length=100)
     strap_material = models.CharField(max_length=100)
     movement_type = models.CharField(50, choices=movement_type)
-    water_resistance = models.IntegerField(null=True, blank=True)
-    case_diameter = models.IntegerField(blank=True, null=True)
-    case_thickness = models.IntegerField(blank=True, null=True)
-    bandwidth = models.IntegerField(blank=True, null=True)
+    water_resistance = models.CharField(max_length=100)
+    case_diameter = models.FloatField()
+    case_thickness = models.FloatField()
+    bandwidth = models.IntegerField()
     dial_color = models.CharField(max_length=150)
     crystal_material = models.CharField(max_length=100)
-    complications = models.CharField(max_length=255, blank=True, null=True)
-    power_reserve = models.IntegerField()
-    price = models.FloatField()
+    complications = models.CharField(max_length=255)
+    power_reserve = models.CharField(max_length =100)
+    price = models.CharField(blank=True, null=True, max_length=100)
+    date = models.DateTimeField()
+    class Meta:
+        ordering = ['id']
 
+    def __str__(self):
+        return f' {self.id}, {self.brand}'
 
 
